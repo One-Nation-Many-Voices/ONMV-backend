19ad9cbb4b6c27680bc22d6f6eea3d7cfc2db264
Charlotte Moore <mooc123@gmail.com>
2017-02-20 12:33:46 -0700

    adds access-controll-allow-origin header as before action to controller

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,5 +1,11 @@
 class Api::V1::EventsController < ApplicationController
 
+  before_filter :set_headers
+  protected
+  def set_headers
+    response.headers['Access-Control-Allow-Origin'] = '*'
+  end
+
   def index
     render json: Event.all
   end
@@ -7,5 +13,7 @@ class Api::V1::EventsController < ApplicationController
   def show
     render json: Event.find(params[:id])
   end
+
+
 
 end
